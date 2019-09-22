3ac09ef24599b82b9b95b1faf16d22b4cf134fb7
fufufukakaka <demonstrundum@gmail.com>
2019-09-22 00:53:30 +0000

    aggregate ML-20M

--- lib/recommend_util.py
+++ lib/recommend_util.py
@@ -2,38 +2,6 @@ import scipy.sparse as sps
 import numpy as np
 
 
-def ndcg(ranked_list, pos_items, relevance=None, at=None):
-
-    if relevance is None:
-        relevance = np.ones_like(pos_items)
-    assert len(relevance) == pos_items.shape[0]
-
-    # Create a dictionary associating item_id to its relevance
-    # it2rel[item] -> relevance[item]
-    it2rel = {it: r for it, r in zip(pos_items, relevance)}
-
-    # Creates array of length "at" with the relevance associated to the item in that position
-    rank_scores = np.asarray([it2rel.get(it, 0.0) for it in ranked_list[:at]], dtype=np.float32)
-
-    # IDCG has all relevances to 1, up to the number of items in the test set
-    ideal_dcg = dcg(np.sort(relevance)[::-1])
-
-    # DCG uses the relevance of the recommended items
-    rank_dcg = dcg(rank_scores)
-
-    if rank_dcg == 0.0:
-        return 0.0
-
-    ndcg_ = rank_dcg / ideal_dcg
-    # assert 0 <= ndcg_ <= 1, (rank_dcg, ideal_dcg, ndcg_)
-    return ndcg_
-
-
-def dcg(scores):
-    return np.sum(np.divide(np.power(2, scores) - 1, np.log(np.arange(scores.shape[0], dtype=np.float32) + 2)),
-                  dtype=np.float32)
-
-
 class IncrementalSparseMatrix_ListBased(object):
 
     def __init__(self, auto_create_col_mapper = False, auto_create_row_mapper = False, n_rows = None, n_cols = None):
@@ -450,3 +418,34 @@ def split_train_validation_leave_one_out_user_wise(URM_train, verbose=True, at_l
 
 
     return URM_train, URM_validation
+
+def ndcg(ranked_list, pos_items, relevance=None, at=None):
+
+    if relevance is None:
+        relevance = np.ones_like(pos_items)
+    assert len(relevance) == pos_items.shape[0]
+
+    # Create a dictionary associating item_id to its relevance
+    # it2rel[item] -> relevance[item]
+    it2rel = {it: r for it, r in zip(pos_items, relevance)}
+
+    # Creates array of length "at" with the relevance associated to the item in that position
+    rank_scores = np.asarray([it2rel.get(it, 0.0) for it in ranked_list[:at]], dtype=np.float32)
+
+    # IDCG has all relevances to 1, up to the number of items in the test set
+    ideal_dcg = dcg(np.sort(relevance)[::-1])
+
+    # DCG uses the relevance of the recommended items
+    rank_dcg = dcg(rank_scores)
+
+    if rank_dcg == 0.0:
+        return 0.0
+
+    ndcg_ = rank_dcg / ideal_dcg
+    # assert 0 <= ndcg_ <= 1, (rank_dcg, ideal_dcg, ndcg_)
+    return ndcg_
+
+
+def dcg(scores):
+    return np.sum(np.divide(np.power(2, scores) - 1, np.log(np.arange(scores.shape[0], dtype=np.float32) + 2)),
+                  dtype=np.float32)
